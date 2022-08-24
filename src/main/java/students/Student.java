4a77cffb2487881cbdfe348801a26f14c129f62c
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-08-24 09:23:42 -0500

    student class and test done

--- src/main/java/students/Student.java
+++ src/main/java/students/Student.java
@@ -1,4 +1,63 @@
 package students;
+import java.util.ArrayList;
 
 public class Student {
+    private  String name;
+    private long id;
+    public ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.id=id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // returns the student's id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int ...grade){
+        for (int j : grade) {
+            grades.add(j);
+        }
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        //sum of all grades divided by quantity of grades
+        int gradesArray = 0;
+        int gradeAverage = 0;
+        for(int i = 0; i< grades.size(); i++){
+            gradesArray += grades.get(i);
+            gradeAverage = gradesArray / grades.size();
+        }
+        return gradeAverage;
+    }
 }
+
+
+
--- src/main/test/java/students/StudentTest.java
+++ src/main/test/java/students/StudentTest.java
@@ -1,4 +1,43 @@
 package students;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class StudentTest {
+    private Student student;
+
+    @Before
+    public void setUp(){
+        student = new Student("Bob", 7);
+        student.addGrade(78,89,90);
+    }
+
+    @Test
+    public void  testGetID(){
+        assertEquals(7, student.getId());
+    }
+    @Test
+    public void testgetName() {
+        assertEquals("Bob", student.getName());
+    }
+
+    @Test
+    public void testAddGrade(){
+        assertEquals(3, student.getGrades().size());
+    }
+
+    @Test
+    public void testGetGrades(){
+//       int[] gradesArray = new int [] {78,89,90};
+//        assertSame(78, student.getGrades().get(0));
+        assertEquals("[78, 89, 90]", student.getGrades().toString());
+        System.out.println(student.getGrades().toString());
+    }
+
+    @Test
+    public void testGetGradeAverage(){
+//        student.addGrade(77,89);
+        assertEquals(85.0, student.getGradeAverage());
+    }
 }
